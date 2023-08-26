9056423be952c6eaa90044fecc13ceb39e947f4c
JosuehCA <josuemago2012@gmail.com>
2023-08-26 04:29:19 -0600

    Código listo

--- Lista_Josueh.c
+++ Lista_Josueh.c
@@ -8,22 +8,37 @@ typedef struct {
     int semestre;
 } alumno;
 
+typedef alumno* alumnoptr;
+
 typedef struct nodo {
-    alumno* alumno;
+    alumnoptr alumno;
     struct nodo* enlace;
 } nodo;
 
 typedef nodo* nodoptr;
-typedef alumno* alumnoptr;
 
 alumnoptr crearAlumno(char*, int, int);
 void imprimirAlumno(alumnoptr);
 nodoptr crearNodo(alumnoptr);
-void insertarNodoOrdenadoCreditos(nodoptr*, alumnoptr);
+void insertarNodoOrdenadoCreditos(nodoptr*, nodoptr);
 void imprimirLista(nodoptr*);
 
 int main(){
+    nodoptr cabecera = NULL;
 
+    alumnoptr alumno1 = crearAlumno("Josueh", 35, 6);
+    alumnoptr alumno2 = crearAlumno("Ricardo", 20, 3);
+    alumnoptr alumno3 = crearAlumno("Deco", 25, 8);
+    alumnoptr alumno4 = crearAlumno("Andrea", 40, 2);
+    alumnoptr alumno5 = crearAlumno("Mariano", 38, 9);
+
+    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(alumno1));
+    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(alumno2));
+    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(alumno3));
+    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(alumno4));
+    insertarNodoOrdenadoCreditos(&cabecera, crearNodo(alumno5));
+
+    imprimirLista(&cabecera);
 
     return 0;
 }
@@ -42,17 +57,34 @@ alumnoptr crearAlumno(char* NombreCompleto, int creditos_aprobados, int semestre
 nodoptr crearNodo(alumnoptr alumno){
     nodoptr nuevo;
 
+    nuevo = (nodoptr)malloc(sizeof(nodo));
     nuevo -> alumno = alumno;
     nuevo -> enlace = NULL;
 
     return nuevo;
 };
 
-void insertarNodoOrdenadoCreditos(nodoptr* cabecera, alumnoptr nuevo){
+void insertarNodoOrdenadoCreditos(nodoptr* cabecera, nodoptr nuevo){
+    nodoptr anterior = NULL;
     nodoptr actual = *cabecera;
 
-    while(actual != NULL){
-        
+    if(*cabecera == NULL){      // Si la lista está vacía
+        *cabecera = nuevo;
+    }
+    else {
+        while(actual != NULL && ((nuevo -> alumno -> creditos_aprobados) < (actual -> alumno -> creditos_aprobados))){
+            anterior = actual;
+            actual = actual -> enlace;
+        }
+
+        if(anterior == NULL){       // Si es el primer elemento
+        nuevo -> enlace = *cabecera;
+        *cabecera = nuevo;         //(???)
+        }  
+        else {      // Si está en medio o al final
+            anterior -> enlace = nuevo;
+            nuevo -> enlace = actual;
+        }
     }
 };
 
